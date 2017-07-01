537cde2789b746c8fe7a90232fd8dac3443472ef
曾義格 <tsengeagle@gmail.com>
2017-07-01 11:11:20 +0800

    建立第一個情境

---
+++ GOOS_SampleTests/Features/BudgetCreate.feature
@@ -0,0 +1,11 @@
+﻿Feature: BudgetCreate
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Success add a budget
+
+Given go to adding budget page
+When I add a buget 2000 for "2017-10"
+Then it should display "added successfully"
---
+++ GOOS_SampleTests/Steps/BudgetCreateSteps.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using FluentAutomation;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.Steps
+{
+    [Binding]
+    [Scope(Feature = "BudgetCreate")]
+    public class BudgetCreateSteps:FluentTest
+    {
+        private BudgetCreatePage _budgetCreatePage;
+
+        public BudgetCreateSteps()
+        {
+            this._budgetCreatePage = new BudgetCreatePage(this);
+        }
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        [Given(@"go to adding budget page")]
+        public void GivenGoToAddingBudgetPage()
+        {
+            this._budgetCreatePage.Go();
+        }
+        
+        [When(@"I add a buget (.*) for ""(.*)""")]
+        public void WhenIAddABugetFor(int amount, string yearMonth)
+        {
+
+            this._budgetCreatePage
+                .Amount(amount)
+                .Month(yearMonth)
+                .AddBudget();
+        }
+        
+        [Then(@"it should display ""(.*)""")]
+        public void ThenItShouldDisplay(string message)
+        {
+            this._budgetCreatePage.ShouldDisplay(message);
+        }
+    }
+
+
+    internal class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+        }
+
+        public BudgetCreatePage Amount(int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BudgetCreatePage Month(string yearMonth)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BudgetCreatePage AddBudget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BudgetCreatePage ShouldDisplay(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
